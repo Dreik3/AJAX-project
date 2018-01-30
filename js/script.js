95c369630ac0ec9f3a2f30b57d477d36b3df030a
Dreik3 <nik.dolmatov@gmail.com>
2018-01-30 22:56:14 +0300

    added background image from GMaps

--- js/script.js
+++ js/script.js
@@ -12,6 +12,20 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    //$body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location= %streetData%, %cityData%">');
+
+    /*var streetStr = document.getElementById("street").value;*/
+    /*var cityStr = document.getElementById("city").value;*/
+    /*console.log(streetStr, cityStr);*/
+
+    btn.onclick = function replaceData(){
+        debugger;
+        var backGroundImage = '<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=%streetData%, %cityData%">';
+        var formattedBackGroundImage0 = backGroundImage.replace("%streetData%", document.getElementById("street").value);
+        var formattedBackGroundImage1 = formattedBackGroundImage0.replace("%cityData%", document.getElementById("city").value);
+        $body.append(formattedBackGroundImage1);
+
+    };
 
     // YOUR CODE GOES HERE!
 
