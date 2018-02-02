0d04957d475d49dffe883f48fd11911787663b3c
Dreik3 <nik.dolmatov@gmail.com>
2018-02-03 00:30:27 +0300

    added ajax request for wikipedia articles

--- js/script.js
+++ js/script.js
@@ -30,32 +30,52 @@ function loadData() {
         var formattedBackGroundImage0 = backGroundImage.replace("%streetData%", document.getElementById("street").value);
         var formattedBackGroundImage1 = formattedBackGroundImage0.replace("%cityData%", document.getElementById("city").value);
         $body.append(formattedBackGroundImage1);
-
     };*/
 
     // NYTimes AJAX request
     var articles = [];
-    var nytimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=f86c9602f8fb402fbd09ba323bfbe585";
+    var nytimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+                     cityStr + "&sort=newest&api-key=f86c9602f8fb402fbd09ba323bfbe585";
+
     $.getJSON(nytimesUrl, function(data){
-        debugger;
-        console.log(data.response);
-        console.log("ddd");
+        /*console.log(data.response);*/
         $nytHeaderElem.text("New York Times Artices About " + cityStr);
         articles = data.response.docs;
         for (var i = 0; i < articles.length; i++){
             var article = articles[i];
-            $nytElem.append('<li class="artcles"> <a href="' + article.web_url + '">' + article.headline.main + '</a><p>' + article.snippet + '</p></li>');
-        }
-       /* var items = [];
-        $.each(data.response.docs, function(key, val){
-            items.push( "<li id='" + key + "'>" + val + "</li>" );
-        });*/
-
-     $( "<ul/>", {
-        "class": "article-list",
-        html: items.join( "" )
-      }).appendTo( "body" );
+            $nytElem.append('<li class="articles"> <a href="' + article.web_url + '">' +
+                            article.headline.main + '</a><p>' + article.snippet + '</p></li>');
+        };
+    })
+    .error(function(){
+        $nytHeaderElem.text("New York Times Artices Could Not Be Loaded");
     });
+    var articleStr="";
+    var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search="
+                  + cityStr + '&format=json&callback=wikiCallback';
+    //Wikipedia AJAX reequest
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        success: function(response){
+            var articleList = response[1];
+            for (var i = 0; i < articleList.length; i++){
+                console.log(response);
+                articleStr = articleList[i];
+                var url = "https://en.wikipedia.org/wiki/" + articleStr;
+                $wikiElem.append('<li> <a href=' + url + '">' +
+                            articleStr + '</a></li>');
+            };
+
+        }
+    });
+
+
+
+
+
+
+
     return false;
 };
 
